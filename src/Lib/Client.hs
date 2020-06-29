16d0bccca751927174f25a3499146c724d5a23de
Eric Arbour <arboureric@gmail.com>
2020-06-29 19:54:28 -0400

    Use GetOpt to process command line args and set up help doc.

--- src/Lib/Client.hs
+++ src/Lib/Client.hs
@@ -9,12 +9,16 @@ import Control.Exception (Exception, finally, throw)
 import Control.Monad (forever)
 import Control.Monad.Trans (liftIO)
 import Data.ByteString.Lazy (ByteString)
-import Data.List (intersperse)
+import Data.List (find, intersperse)
 import Data.Maybe (maybe)
 import Data.Proxy (Proxy(..))
 import Data.Typeable (Typeable)
 import Network.HTTP.Client (Manager, defaultManagerSettings, newManager)
 import Network.Socket (withSocketsDo)
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.Exit
+import System.IO
 import Text.Read (readMaybe)
 
 import qualified Data.Aeson as A
@@ -37,7 +41,7 @@ import Lib.Shared
 
 data Strategy =
   Default
-  deriving (Eq, Show)
+  deriving (Eq, Read, Show)
 
 data MoveAgainst =
   MoveAgainst PlayerId Move
@@ -105,6 +109,56 @@ getWSStream = do
 decodeOrFail :: ByteString -> IO PublicEvent
 decodeOrFail btstr = maybe (throw $ InvalidEvent btstr) return (A.decode btstr)
 
+-- Command Line Args
+-------------------------------------------------------------------------------
+data Flag
+  = StrategyArg String
+  | Help
+  deriving (Eq, Show)
+
+options :: [OptDescr Flag]
+options =
+  [ Option
+      ['s']
+      ["strategy"]
+      (ReqArg StrategyArg "Strategy")
+      ("The strategy the client will use when playing the game.\n" <>
+       "Valid strategies are:\n" <>
+       "Defect\n" <>
+       "Cooperate\n" <>
+       "Random5050\n" <>
+       "Random8020\n" <>
+       "Random9010\n" <>
+       "TitForTat\n" <>
+       "TitForTwoTats\n" <>
+       "Vigilante\n" <>
+       "ForgivingTitForTat\n" <>
+       "Ostracise")
+  , Option [] ["help"] (NoArg Help) "Print this help message."
+  ]
+
+processArgs :: IO Strategy
+processArgs = do
+  args <- getArgs
+  case getOpt Permute options args of
+    (opts, _, [])
+      | (Help `elem` opts) -> do
+        hPutStrLn stdout (usageInfo header options)
+        exitWith ExitSuccess
+      | not (any isStrategyArg opts) -> do
+        hPutStrLn
+          stderr
+          ("Please provide a strategy.\n" <> usageInfo header options)
+        exitWith (ExitFailure 1)
+      | otherwise -> return Default
+    (_, _, errs) -> do
+      hPutStrLn stderr (concat errs <> usageInfo header options)
+      exitWith (ExitFailure 1)
+  where
+    header = "Usage: client-exe [OPTION...]"
+    isStrategyArg (StrategyArg _) = True
+    isStrategyArg _ = False
+
 -- Game
 -------------------------------------------------------------------------------
 eventHandler ::
@@ -150,6 +204,7 @@ getMove moveMap opponent = Defect
 --------------------------------------------------------------------------------
 runClient :: IO ()
 runClient = do
+  processArgs
   maybeDecompStream <- S.uncons getWSStream
   (initialData, streamTail) <-
     maybe (throw NoStreamFound) return maybeDecompStream
